22f9dd80e168fe23e9b01f88299d019d57ed6613
sonambaj <102884081+sonambaj@users.noreply.github.com>
2022-04-03 01:27:27 +0100

    Add files via upload

---
+++ advising.js
@@ -0,0 +1,522 @@
+function changeLaugh1(){
+	var laughImage1 = document.getElementById('laughImage1');
+	
+	console.log(laughImage1.src);
+	console.log(laughImage1.src.includes("laugh_blue.png"));
+	if(laughImage1.src.includes("laugh_blue.png") == true){
+document.getElementById('laughImage1').src='laugh_colour.png'; 
+document.getElementById('FineImage1').src='fine_blue.png';
+document.getElementById('SadImage1').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('laughImage1').src='laugh_blue.png'; 
+}
+stateHandle();
+}    
+
+function changeLaugh2(){
+	var laughImage2 = document.getElementById('laughImage2');
+	
+	console.log(laughImage2.src);
+	console.log(laughImage2.src.includes("laugh_blue.png"));
+	if(laughImage2.src.includes("laugh_blue.png") == true){
+document.getElementById('laughImage2').src='laugh_colour.png'; 
+document.getElementById('FineImage2').src='fine_blue.png';
+document.getElementById('SadImage2').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('laughImage2').src='laugh_blue.png'; 
+}
+stateHandle();
+}
+
+function changeLaugh3(){
+	var laughImage3 = document.getElementById('laughImage3');
+	console.log(laughImage3.src);
+	console.log(laughImage3.src.includes("laugh_blue.png"));
+	if(laughImage3.src.includes("laugh_blue.png") == true){
+document.getElementById('laughImage3').src='laugh_colour.png'; 
+document.getElementById('FineImage3').src='fine_blue.png';
+document.getElementById('SadImage3').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('laughImage3').src='laugh_blue.png'; 
+}
+stateHandle();
+}
+
+function changeLaugh4(){
+	var laughImage4 = document.getElementById('laughImage4');
+	
+	console.log(laughImage4.src);
+	console.log(laughImage4.src.includes("laugh_blue.png"));
+	if(laughImage4.src.includes("laugh_blue.png") == true){
+document.getElementById('laughImage4').src='laugh_colour.png'; 
+document.getElementById('FineImage4').src='fine_blue.png';
+document.getElementById('SadImage4').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('laughImage4').src='laugh_blue.png'; 
+}
+stateHandle();
+}
+
+function changeLaugh5(){
+	var laughImage5 = document.getElementById('laughImage5');
+	
+	console.log(laughImage5.src);
+	console.log(laughImage5.src.includes("laugh_blue.png"));
+	if(laughImage5.src.includes("laugh_blue.png") == true){
+document.getElementById('laughImage5').src='laugh_colour.png'; 
+document.getElementById('FineImage5').src='fine_blue.png';
+document.getElementById('SadImage5').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('laughImage5').src='laugh_blue.png'; 
+}
+stateHandle();
+}
+
+function changeLaugh6(){
+	var laughImage6 = document.getElementById('laughImage6');
+	
+	console.log(laughImage6.src);
+	console.log(laughImage6.src.includes("laugh_blue.png"));
+	if(laughImage6.src.includes("laugh_blue.png") == true){
+document.getElementById('laughImage6').src='laugh_colour.png'; 
+document.getElementById('FineImage6').src='fine_blue.png';
+document.getElementById('SadImage6').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('laughImage6').src='laugh_blue.png'; 
+}
+stateHandle();
+}
+
+function changeLaugh7(){
+	var laughImage7 = document.getElementById('laughImage7');
+	
+	console.log(laughImage7.src);
+	console.log(laughImage7.src.includes("laugh_blue.png"));
+	if(laughImage7.src.includes("laugh_blue.png") == true){
+document.getElementById('laughImage7').src='laugh_colour.png'; 
+document.getElementById('FineImage7').src='fine_blue.png';
+document.getElementById('SadImage7').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('laughImage2').src='laugh_blue.png'; 
+}
+stateHandle();
+}
+
+function changeLaugh8(){
+	var laughImage8 = document.getElementById('laughImage8');
+	
+	console.log(laughImage8.src);
+	console.log(laughImage8.src.includes("laugh_blue.png"));
+	if(laughImage8.src.includes("laugh_blue.png") == true){
+document.getElementById('laughImage8').src='laugh_colour.png'; 
+document.getElementById('FineImage8').src='fine_blue.png';
+document.getElementById('SadImage7').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('laughImage8').src='laugh_blue.png'; 
+}
+stateHandle();
+}
+
+
+
+
+
+
+
+
+
+
+function changeFine1(){
+	var FineImage1 = document.getElementById('FineImage1');
+	
+	console.log(FineImage1.src);
+	console.log(FineImage1.src.includes("fine_blue.png"));
+	if(FineImage1.src.includes("fine_blue.png") == true){
+document.getElementById('FineImage1').src='fine_colour.png'; 
+document.getElementById('laughImage1').src='laugh_blue.png';
+document.getElementById('SadImage1').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('FineImage1').src='fine_blue.png'; 
+}
+stateHandle();
+}    
+
+function changeFine2(){
+	var FineImage2 = document.getElementById('FineImage2');
+	
+	console.log(FineImage2.src);
+	console.log(FineImage2.src.includes("fine_blue.png"));
+	if(FineImage2.src.includes("fine_blue.png") == true){
+document.getElementById('FineImage2').src='fine_colour.png'; 
+document.getElementById('laughImage2').src='laugh_blue.png';
+document.getElementById('SadImage2').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('FineImage2').src='fine_blue.png'; 
+}
+stateHandle();
+}
+
+function changeFine3(){
+	var FineImage3 = document.getElementById('FineImage3');
+	
+	console.log(FineImage3.src);
+	console.log(FineImage3.src.includes("fine_blue.png"));
+	if(FineImage3.src.includes("fine_blue.png") == true){
+document.getElementById('FineImage3').src='fine_colour.png'; 
+document.getElementById('laughImage3').src='laugh_blue.png';
+document.getElementById('SadImage3').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('FineImage3').src='fine_blue.png'; 
+}
+stateHandle();
+}
+
+function changeFine4(){
+	var FineImage4 = document.getElementById('FineImage4');
+	
+	console.log(FineImage4.src);
+	console.log(FineImage4.src.includes("fine_blue.png"));
+	if(FineImage4.src.includes("fine_blue.png") == true){
+document.getElementById('FineImage4').src='fine_colour.png'; 
+document.getElementById('laughImage4').src='laugh_blue.png';
+document.getElementById('SadImage4').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('FineImage4').src='fine_blue.png'; 
+}
+stateHandle();
+}
+
+function changeFine5(){
+	var FineImage5 = document.getElementById('FineImage5');
+	
+	console.log(FineImage5.src);
+	console.log(FineImage5.src.includes("fine_blue.png"));
+	if(FineImage5.src.includes("fine_blue.png") == true){
+document.getElementById('FineImage5').src='fine_colour.png'; 
+document.getElementById('laughImage5').src='laugh_blue.png';
+document.getElementById('SadImage5').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('FineImage5').src='fine_blue.png'; 
+}
+stateHandle();
+}
+
+function changeFine6(){
+	var FineImage6 = document.getElementById('FineImage6');
+	
+	console.log(FineImage6.src);
+	console.log(FineImage6.src.includes("fine_blue.png"));
+	if(FineImage6.src.includes("fine_blue.png") == true){
+document.getElementById('FineImage6').src='fine_colour.png'; 
+document.getElementById('laughImage6').src='laugh_blue.png';
+document.getElementById('SadImage6').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('FineImage6').src='fine_blue.png'; 
+}
+stateHandle();
+}
+
+function changeFine7(){
+	var FineImage7 = document.getElementById('FineImage7');
+	
+	console.log(FineImage7.src);
+	console.log(FineImage7.src.includes("fine_blue.png"));
+	if(FineImage7.src.includes("fine_blue.png") == true){
+document.getElementById('FineImage7').src='fine_colour.png'; 
+document.getElementById('laughImage7').src='laugh_blue.png';
+document.getElementById('SadImage7').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('FineImage7').src='fine_blue.png'; 
+}
+stateHandle();
+}
+
+function changeFine8(){
+	var FineImage8 = document.getElementById('FineImage8');
+	
+	console.log(FineImage8.src);
+	console.log(FineImage8.src.includes("fine_blue.png"));
+	if(FineImage8.src.includes("fine_blue.png") == true){
+document.getElementById('FineImage8').src='fine_colour.png'; 
+document.getElementById('laughImage8').src='laugh_blue.png';
+document.getElementById('SadImage8').src='sad_blue.png';
+}
+else
+{
+	document.getElementById('FineImage8').src='fine_blue.png'; 
+}
+stateHandle();
+}
+
+
+
+
+
+
+
+
+
+
+function changeSad1(){
+	var SadImage1 = document.getElementById('SadImage1');
+	
+	console.log(SadImage1.src);
+	console.log(SadImage1.src.includes("sad_blue.png"));
+	if(SadImage1.src.includes("sad_blue.png") == true){
+document.getElementById('SadImage1').src='sad_colour.png'; 
+document.getElementById('laughImage1').src='laugh_blue.png';
+document.getElementById('FineImage1').src='fine_blue.png';
+}
+else
+{
+	document.getElementById('SadImage1').src='sad_blue.png'; 
+}
+stateHandle();
+}    
+
+function changeSad2(){
+	var SadImage2 = document.getElementById('SadImage2');
+	
+	console.log(SadImage2.src);
+	console.log(SadImage2.src.includes("sad_blue.png"));
+	if(SadImage2.src.includes("sad_blue.png") == true){
+document.getElementById('SadImage2').src='sad_colour.png'; 
+document.getElementById('laughImage2').src='laugh_blue.png';
+document.getElementById('FineImage2').src='fine_blue.png';
+}
+else
+{
+	document.getElementById('SadImage2').src='sad_blue.png'; 
+}
+stateHandle();
+}
+
+function changeSad3(){
+	var SadImage3 = document.getElementById('SadImage3');
+	
+	console.log(SadImage3.src);
+	console.log(SadImage3.src.includes("sad_blue.png"));
+	if(SadImage3.src.includes("sad_blue.png") == true){
+document.getElementById('SadImage3').src='sad_colour.png'; 
+document.getElementById('laughImage3').src='laugh_blue.png';
+document.getElementById('FineImage3').src='fine_blue.png';
+}
+else
+{
+	document.getElementById('SadImage3').src='sad_blue.png'; 
+}
+stateHandle();
+}
+
+function changeSad4(){
+	var SadImage4 = document.getElementById('SadImage4');
+	
+	console.log(SadImage4.src);
+	console.log(SadImage4.src.includes("sad_blue.png"));
+	if(SadImage4.src.includes("sad_blue.png") == true){
+document.getElementById('SadImage4').src='sad_colour.png';
+document.getElementById('laughImage4').src='laugh_blue.png';
+document.getElementById('FineImage4').src='fine_blue.png'; 
+}
+else
+{
+	document.getElementById('SadImage4').src='sad_blue.png'; 
+}
+stateHandle();
+}
+
+function changeSad5(){
+	var SadImage5 = document.getElementById('SadImage5');
+	
+	console.log(SadImage5.src);
+	console.log(SadImage5.src.includes("sad_blue.png"));
+	if(SadImage5.src.includes("sad_blue.png") == true){
+document.getElementById('SadImage5').src='sad_colour.png'
+document.getElementById('laughImage5').src='laugh_blue.png';
+document.getElementById('FineImage5').src='fine_blue.png';; 
+}
+else
+{
+	document.getElementById('SadImage5').src='sad_blue.png'; 
+}
+stateHandle();
+}
+
+function changeSad6(){
+	var SadImage6 = document.getElementById('SadImage6');
+	
+	console.log(SadImage6.src);
+	console.log(SadImage6.src.includes("sad_blue.png"));
+	if(SadImage6.src.includes("sad_blue.png") == true){
+document.getElementById('SadImage6').src='sad_colour.png'; 
+document.getElementById('laughImage6').src='laugh_blue.png';
+document.getElementById('FineImage6').src='fine_blue.png';
+}
+else
+{
+	document.getElementById('SadImage6').src='sad_blue.png'; 
+}
+stateHandle();
+}
+
+function changeSad7(){
+	var SadImage7 = document.getElementById('SadImage7');
+	
+	console.log(SadImage7.src);
+	console.log(SadImage7.src.includes("sad_blue.png"));
+	if(SadImage7.src.includes("sad_blue.png") == true){
+document.getElementById('SadImage7').src='sad_colour.png'; 
+document.getElementById('laughImage7').src='laugh_blue.png';
+document.getElementById('FineImage7').src='fine_blue.png';
+}
+else
+{
+	document.getElementById('SadImage7').src='sad_blue.png'; 
+}
+stateHandle();
+}
+
+function changeSad8(){
+	var SadImage8 = document.getElementById('SadImage8');
+	
+	console.log(SadImage8.src);
+	console.log(SadImage8.src.includes("sad_blue.png"));
+	if(SadImage8.src.includes("sad_blue.png") == true){
+document.getElementById('SadImage8').src='sad_colour.png'; 
+document.getElementById('laughImage8').src='laugh_blue.png';
+document.getElementById('FineImage8').src='fine_blue.png';
+}
+else
+{
+	document.getElementById('SadImage8').src='sad_blue.png'; 
+}
+stateHandle();
+}
+
+
+
+
+function stateHandle() {
+
+let inputSad1 = SadImage1.src.includes("sad_colour.png");
+let inputFine1 = FineImage1.src.includes("fine_colour.png");
+let inputLaugh1 = laughImage1.src.includes("laugh_colour.png");
+
+let inputSad2 = SadImage2.src.includes("sad_colour.png");
+let inputFine2 = FineImage2.src.includes("fine_colour.png");
+let inputLaugh2 = laughImage2.src.includes("laugh_colour.png");
+
+let inputSad3 = SadImage3.src.includes("sad_colour.png");
+let inputFine3 = FineImage3.src.includes("fine_colour.png");
+let inputLaugh3 = laughImage3.src.includes("laugh_colour.png");
+
+let inputSad4 = SadImage4.src.includes("sad_colour.png");
+let inputFine4 = FineImage4.src.includes("fine_colour.png");
+let inputLaugh4 = laughImage4.src.includes("laugh_colour.png");
+
+let inputSad5 = SadImage5.src.includes("sad_colour.png");
+let inputFine5 = FineImage5.src.includes("fine_colour.png");
+let inputLaugh5 = laughImage5.src.includes("laugh_colour.png");
+
+let inputSad6 = SadImage6.src.includes("sad_colour.png");
+let inputFine6 = FineImage6.src.includes("fine_colour.png");
+let inputLaugh6 = laughImage6.src.includes("laugh_colour.png");
+
+let inputSad7 = SadImage7.src.includes("sad_colour.png");
+let inputFine7 = FineImage7.src.includes("fine_colour.png");
+let inputLaugh7 = laughImage7.src.includes("laugh_colour.png");
+
+let inputSad8 = SadImage8.src.includes("sad_colour.png");
+let inputFine8 = FineImage8.src.includes("fine_colour.png");
+let inputLaugh8 = laughImage8.src.includes("laugh_colour.png");
+
+console.log((inputSad1 || inputFine1 || inputLaugh1));
+console.log((inputSad2 || inputFine2 || inputLaugh2));
+console.log((inputSad3 || inputFine3 || inputLaugh3));
+console.log((inputSad4 || inputFine4 || inputLaugh4));
+console.log((inputSad5 || inputFine5 || inputLaugh5));
+console.log((inputSad6 || inputFine6 || inputLaugh6));
+console.log((inputSad7 || inputFine7 || inputLaugh7));
+console.log((inputSad8 || inputFine8 || inputLaugh8));
+
+
+
+if ((inputSad1 || inputFine1 || inputLaugh1) && (inputSad2 || inputFine2 || inputLaugh2) && (inputSad3 || inputFine3 || inputLaugh3) && (inputSad4 || inputFine4 || inputLaugh4) && 
+
+(inputSad5 || inputFine5 || inputLaugh5) && (inputSad6 || inputFine6 || inputLaugh6) && (inputSad7 || inputFine7 || inputLaugh7) && (inputSad8 || inputFine8 || inputLaugh8))
+{
+    document.getElementById('cc').src='button_next.png'; 
+	document.getElementById("cc").disabled = false;
+}
+else
+{
+    document.getElementById('cc').src='button_next_gray.png';
+	document.getElementById("cc").disabled = true;
+}
+}
+
+
+function change() {
+	
+	if (document.getElementById('txt') .value == '')
+{
+    document.getElementById('cng').src='button_next_gray.png'; 
+	document.getElementById("cng").disabled = true;
+}
+else
+{
+    document.getElementById('cng').src='button_next.png';
+	document.getElementById("cng").disabled = false;
+}
+}
+
+function submitButton() {
+	
+	
+	if (((document.getElementById('t1').checked) || (document.getElementById('t2') .checked) || (document.getElementById('t3') .checked) || (document.getElementById('t4') .checked) || (document.getElementById('t5') .checked) || (document.getElementById('t6') .checked) || (document.getElementById('t7') .checked) || (document.getElementById('t8') .checked) || (document.getElementById('t9') .checked) || (document.getElementById('t10') .checked) || (document.getElementById('t11') .checked == ''))
+		
+	&& ((document.getElementById('t20') .checked) || (document.getElementById('t21') .checked) || (document.getElementById('t22') .checked)) 
+	
+	&& ((document.getElementById('t30') .checked) || (document.getElementById('t31') .checked) || (document.getElementById('t32') .checked))
+	
+	&& ((document.getElementById('text') .value != '')))
+{
+    document.getElementById('chan').src='button_submit.png'; 
+	document.getElementById("chan").disabled = false;
+}
+else
+{
+    document.getElementById('chan').src='button_submit_gray.png';
+	document.getElementById("chan").disabled = true;
+}
+}
